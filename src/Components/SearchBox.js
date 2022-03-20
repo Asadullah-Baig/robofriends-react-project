95d6174d11d6a380c96feabfe06b32e2dbceeb0e
Asadullah Baig <61774598+Asadullah-Baig@users.noreply.github.com>
2022-03-20 06:30:08 +0500

    complete project

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React from "react";
 
-
-
-const SearchBox=({Searchfield,searchChange})=>{
-	return(
-		<div className='pa2'>
-		<input
-
-		className='pa3 ba b--green bg-lightest-blue'
-		type='search'
-		placeholder='search robots'
-		onChange={searchChange}
-
-		/>
-		</div>
-
-
-		);
-}
-export default SearchBox;  
+const SearchBox = ({ Searchfield, searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+export default SearchBox;
