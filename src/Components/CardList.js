efc8ced69f46e99a6c08f0d1f5c23942215be546
Asadullah Baig <61774598+Asadullah-Baig@users.noreply.github.com>
2022-03-20 06:25:07 +0500

    complete project

---
+++ src/Components/CardList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots })=>{
+
+	
+
+	return(
+		<div>
+	    {
+	    	robots.map((user,i)=>{
+
+		  return (
+		        <Card
+		        key={1}
+		        id={robots[i].id}
+		        name={robots[i].name}
+		        email={robots[i].email}
+		        />
+		     );
+
+	     })
+
+	     }
+	    </div>
+
+
+		);
+}
+export default CardList;
